f9e4d3adffa4708f70625ea1156bd96544f24ffe
HamInGyu <hfire1207@gmail.com>
2022-02-25 00:05:46 +0900

    feat: 1.3 dispatch와 action에 대해서
    dispatch로 store에 업데이트 요청, action에 어떠한 업데이트를 원하는지 담아서

--- src/index.js
+++ src/index.js
@@ -6,13 +6,20 @@ const $number = document.querySelector("span");
 
 //reducer : state를 변경하는 함수!
 //reducer가 리턴하는건 업데이트 완료된 state
-const countReducer = (state = 0) => {
-  //여기서 state 업데이트하고
-
-  return state; //업데이트된 state 리턴!
+//action.type로 어떤 수정을할지 미리 정의하고, type에 따라 다른 업데이트 수행
+const countReducer = (state = 0, action) => {
+  if (action.type === "PLUS") {
+    return state + 1;
+  } else if (action.type === "MINUS") {
+    return state - 1;
+  } else {
+    return state;
+  }
 };
 
 //createStore = state를 관리하는 공간을 만들어줌
 const countStore = createStore(countReducer); //인자로 리듀서 들어가야해
 
+console.log(countStore.getState()); //state를 가져온다!
+countStore.dispatch({ type: "PLUS" }); //store에 업데이트 요청!
 console.log(countStore.getState()); //state를 가져온다!
