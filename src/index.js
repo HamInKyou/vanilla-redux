c506b9cee1f73603582c652bd1074da03c519959
HamInGyu <hfire1207@gmail.com>
2022-02-25 00:37:41 +0900

    feat: 1.5 리덕스 예제 개선하기

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ const $plus = document.querySelector("#plus");
 const $minus = document.querySelector("#minus");
 const $number = document.querySelector("span");
 
+const ADD = "ADD"; //상수화로 미리 변수로 설정해두기
+const MINUS = "MINUS";
+
 //reducer : state를 변경하는 함수!
 //reducer가 리턴하는건 업데이트 완료된 state
 //action.type로 어떤 수정을할지 미리 정의하고, type에 따라 다른 업데이트 수행
 const countReducer = (state = 0, action) => {
-  if (action.type === "PLUS") {
-    return state + 1;
-  } else if (action.type === "MINUS") {
-    return state - 1;
-  } else {
-    return state;
+  switch (action.type) {
+    case ADD:
+      return state + 1;
+    case MINUS:
+      return state - 1;
+    default:
+      return state;
   }
 };
 
@@ -27,9 +31,9 @@ countStore.subscribe(() => {
 });
 
 $plus.addEventListener("click", () => {
-  countStore.dispatch({ type: "PLUS" });
+  countStore.dispatch({ type: ADD });
 });
 
 $minus.addEventListener("click", () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 });
