2eb34c628f4647df9cf2fe300df2ac2fb6170575
HamInGyu <hfire1207@gmail.com>
2022-02-25 10:27:15 +0900

    feat: 2.1 toDo 리덕스 초기 세팅
    1) store 만들기
    2) reducer 만들기
    3) dispatch로 toDo 생성 요청

--- src/index.js
+++ src/index.js
@@ -1,39 +1,28 @@
 import { createStore } from "redux";
 
-const $plus = document.querySelector("#plus");
-const $minus = document.querySelector("#minus");
-const $number = document.querySelector("span");
+const $form = document.querySelector("form");
+const $input = document.querySelector("input");
+const $ul = document.querySelector("ul");
 
-const ADD = "ADD"; //상수화로 미리 변수로 설정해두기
-const MINUS = "MINUS";
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-//reducer : state를 변경하는 함수!
-//reducer가 리턴하는건 업데이트 완료된 state
-//action.type로 어떤 수정을할지 미리 정의하고, type에 따라 다른 업데이트 수행
-const countReducer = (state = 0, action) => {
+const todoReducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return state + 1;
-    case MINUS:
-      return state - 1;
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
     default:
       return state;
   }
 };
 
-//createStore = state를 관리하는 공간을 만들어줌
-const countStore = createStore(countReducer); //인자로 리듀서 들어가야해
+const todoStore = createStore(todoReducer);
 
-//subscribe는 우리가 store 안에 있는 변화들을 알 수 있게 해준다.
-//변화를 인지하면 콜백함수 실행하게!
-countStore.subscribe(() => {
-  $number.innerText = countStore.getState();
-});
-
-$plus.addEventListener("click", () => {
-  countStore.dispatch({ type: ADD });
-});
-
-$minus.addEventListener("click", () => {
-  countStore.dispatch({ type: MINUS });
+$form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const toDo = $input.value;
+  $input.value = "";
+  todoStore.dispatch({ type: ADD_TODO });
 });
