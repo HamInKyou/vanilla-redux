7f7344d3db4709b162a8f251428cc8bacd08e1a5
HamInGyu <hfire1207@gmail.com>
2022-02-25 10:39:54 +0900

    feat: 2.2 toDo 리듀서  add_todo 액션 세팅

--- src/index.js
+++ src/index.js
@@ -7,10 +7,14 @@ const $ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+//state는 readOnly -> getState 이런걸로 바로 변경할 수 없어!
+//오로지 액션을 통해서만 state가 변경되어야해
+//깊은 복사 사용 -> 기존거를 업데이트한 걸 리턴하는게 아니라,
+//기존거를 업데이트한 별개의 새로운 데이터를 리턴해야해!
 const todoReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [...state, { text: action.payload, id: Date.now() }];
     case DELETE_TODO:
       return [];
     default:
@@ -24,5 +28,9 @@ $form.addEventListener("submit", (event) => {
   event.preventDefault();
   const toDo = $input.value;
   $input.value = "";
-  todoStore.dispatch({ type: ADD_TODO });
+  todoStore.dispatch({ type: ADD_TODO, payload: toDo });
+});
+
+todoStore.subscribe(() => {
+  console.log(todoStore.getState());
 });
