08bb1d7d91d79f7697aba3526accc3446dc29723
HamInGyu <hfire1207@gmail.com>
2022-02-26 20:54:32 +0900

    feat:3.1 스토어 세팅
    스토어 만들고, Provider로 App에 변경사항 받아들일 Provider로 감싸기

---
+++ src/store.js
@@ -0,0 +1,32 @@
+import { createStore } from "redux";
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+export const addToDo = (text) => {
+  return {
+    type: ADD,
+    payload: text,
+  };
+};
+export const deleteToDo = (id) => {
+  return {
+    type: DELETE,
+    payload: id,
+  };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case DELETE:
+      return state.filter((toDo) => toDo !== action.payload);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+export default store;
