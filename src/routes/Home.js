260b88f7e9c1902a0367580d238cb90816e11dc4
HamInGyu <hfire1207@gmail.com>
2022-02-26 21:31:55 +0900

    feat:3.3 Home 컴포넌트에 dispatch도 연결하기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home(props) {
-  console.log(props);
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(event) {
     setText(event.target.value);
   }
   function onSubmit(event) {
     event.preventDefault();
-    console.log(text);
+    addToDo(text);
+    setText("");
   }
   return (
     <>
@@ -18,16 +19,21 @@ function Home(props) {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-function getCurrentState(state, ownProps) {
+//store에 state를 컴포넌트에 props로 전달해주기 위한 함수
+function mapStateToProps(state, ownProps) {
   //state는 store에 담겨있는 state
   //ownProps는 Home컴포넌트에 전달되는 Props
-  return { state };
+  return { toDos: state };
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return { addToDo: (text) => dispatch(actionCreators.addToDo(text)) };
 }
 
 //connect함수를 쓰면 컴포넌트로 보내는 props에 위 함수에서 리턴한 것들 props로 추가할 수 있게 해준다!
-export default connect(getCurrentState)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/store.js
+++ src/store.js
@@ -29,4 +29,8 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+export const actionCreators = {
+  addToDo,
+  deleteToDo,
+};
 export default store;
