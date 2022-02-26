452bcda6216b1e5bce5f36530e8577caebfd10cb
HamInGyu <hfire1207@gmail.com>
2022-02-26 21:11:41 +0900

    feat:3.2 Home 컴포넌트에 store 연결하기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home(props) {
+  console.log(props);
   const [text, setText] = useState("");
   function onChange(event) {
     setText(event.target.value);
@@ -16,8 +18,16 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
+      <ul></ul>
     </>
   );
 }
 
-export default Home;
+function getCurrentState(state, ownProps) {
+  //state는 store에 담겨있는 state
+  //ownProps는 Home컴포넌트에 전달되는 Props
+  return { state };
+}
+
+//connect함수를 쓰면 컴포넌트로 보내는 props에 위 함수에서 리턴한 것들 props로 추가할 수 있게 해준다!
+export default connect(getCurrentState)(Home);
